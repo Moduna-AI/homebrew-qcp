73837fb762cbf0786d34f50cdb3245368bad7c50
Ashwin Thinnappan <balathinnappan@gmail.com>
2026-06-21 22:47:34 +0530

    Update qcp.rb

--- Formula/qcp.rb
+++ Formula/qcp.rb
@@ -18,7 +18,7 @@ class Qcp < Formula
     
     # 2. Tell the internal pip to pull your app and its dependencies from PyPI
     # Using --no-binary ensures it compiles neatly for the user's system architecture
-    venv.pip_install "qcp-cli==#{version}", "--uploaded-prior-to=2030-01-01T00:00:00Z"
+    venv.pip_install ["qcp-cli==#{version}", "--uploaded-prior-to=2030-01-01T00:00:00Z"]
     
     # 3. Create a global symlink into Homebrew's binary directory
     bin.install_symlink libexec/"bin/qcp"
