5c72f1cf3624cc67c143aac40b52ea4629ff6ef5
Ashwin Thinnappan <balathinnappan@gmail.com>
2026-06-21 23:13:14 +0530

    Update qcp.rb

--- Formula/qcp.rb
+++ Formula/qcp.rb
@@ -10,10 +10,10 @@ class Qcp < Formula
   sha256 "4885fdcc9a05cc9aefb5ad26a8b86267b3c88f70c3afcf43ee82a737c72b1d78"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "python@3.13"
 
   def install
-    venv = virtualenv_create(libexec, "python3.12")
+    venv = virtualenv_create(libexec, "python3.13")
     venv.pip_install buildpath
     bin.install_symlink libexec/"bin/qcp"
   end
